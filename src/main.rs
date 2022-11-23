083ebbf18deb5f7b9a5d44e4bb815e55c0ab2e43
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2022-11-23 22:02:33 +0800

    meson build

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use filedialogshana::choose_file;
 use shanatypes::{OpenFileOptions, SaveFileOptions, SelectedFiles};
-use std::{error::Error, future::pending};
+use std::{collections::HashMap, error::Error, future::pending};
 use tracing::{info, Level};
-use zbus::{dbus_interface, zvariant::ObjectPath, ConnectionBuilder};
+use zbus::{
+    dbus_interface,
+    zvariant::{ObjectPath, OwnedValue, Value},
+    ConnectionBuilder,
+};
 struct Shana;
 //mod filechoosertypes;
 #[dbus_interface(name = "org.freedesktop.impl.portal.FileChooser")]
@@ -13,15 +17,22 @@ impl Shana {
         app_id: String,
         parent_window: String,
         title: String,
-        options: OpenFileOptions,
+        options: HashMap<String, OwnedValue>,
     ) -> (u32, SelectedFiles) {
         println!("handle is {:?}", handle);
         println!("app_id is {}", app_id);
         println!("parent_window is {}", parent_window);
         println!("title is {}", title);
         println!("options is {:?}", options);
-        let selected = options.select_function();
-        choose_file(selected)
+        //let selected = options.select_function();
+        //choose_file(selected)
+        (
+            0,
+            SelectedFiles {
+                uris: vec![],
+                choices: None,
+            },
+        )
     }
 
     async fn save_file(
